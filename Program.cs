0e44ed7652c3d1068cb1cd006a554c30a23dcb94
Nicole <rosanik3103@gmail.com>
2023-09-06 16:31:58 -0300

    Adiciona nome e nome de catálogo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine
+    ("Escreva seu nome");
+
+String nome = Console.ReadLine()!;
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("Escreva seu sobrenome");
+
+String sobrenome = Console.ReadLine()!;
+
+Console.WriteLine
+    ("");
+
+String completo = nome + " " + sobrenome;
+
+Console.WriteLine
+    ("Nome Completo: "
+        +
+            completo);
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("Nome de Catalálogo: "
+        + 
+            $"{sobrenome.ToUpper() + " " + nome}");
