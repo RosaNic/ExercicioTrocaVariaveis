b726c69d31133f2d203c0104b21500815bdda0c7
Nicole <rosanik3103@gmail.com>
2023-09-07 22:09:19 -0300

    corrige o exercicio

--- Program.cs
+++ Program.cs
@@ -1,30 +1,42 @@
 ﻿Console.WriteLine
-    ("Escreva seu nome");
+    ("Escreva o nome");
 
-String nome = Console.ReadLine()!;
-
-Console.WriteLine
-    ("");
-
-Console.WriteLine
-    ("Escreva seu sobrenome");
-
-String sobrenome = Console.ReadLine()!;
-
-Console.WriteLine
-    ("");
-
-String completo = nome + " " + sobrenome;
-
-Console.WriteLine
-    ("Nome Completo: "
-        +
-            completo);
+String nome1 = Console.ReadLine()!;
 
 Console.WriteLine
     ("");
 
 Console.WriteLine
-    ("Nome de Catalálogo: "
-        + 
-            $"{sobrenome.ToUpper() + " " + nome}");
+    ("Escreva o segundo nome");
+
+String nome2 = Console.ReadLine()!;
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("--- Troca de Valores ---");
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("Nome 1...: " + nome1);
+
+Console.WriteLine
+    ("Nome 2...: " + nome2);
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("Após trocar os valores temos:");
+
+Console.WriteLine
+    ("");
+
+Console.WriteLine
+    ("Nome 1...: " + nome2);
+
+Console.WriteLine
+    ("Nome 2...: " + nome1);
